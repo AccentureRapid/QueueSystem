d50649159301c0abd1ff741bfff0efa4489075ca
David <david0718@qq.com>
2018-02-26 18:00:49 +0800

    web api for system

---
+++ Pfizer.QueueSystem.Application/Services/QueueSystemService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.ObjectMapping;
+using Pfizer.QueueSystem.Entities;
+using Pfizer.QueueSystem.Services.Dto;
+
+namespace Pfizer.QueueSystem.Services
+{
+    public class QueueSystemService : IQueueSystemService
+    {
+        private readonly IQueueSystemManager _queueSystemManager;
+        private readonly IObjectMapper _objectMapper;
+        public QueueSystemService(IQueueSystemManager queueSystemManager, 
+            IObjectMapper objectMapper)
+        {
+            _queueSystemManager = queueSystemManager;
+            _objectMapper = objectMapper;
+        }
+
+        public async Task<int> GetOnlineCustomersCount()
+        {
+            var onlineCustomersMinutes = Convert.ToInt32(ConfigurationManager.AppSettings["OnlineCustomerMinutes"]);
+            var count = await _queueSystemManager.GetOnlineCustomersCount(DateTime.UtcNow.AddMinutes(-onlineCustomersMinutes));
+            return count;
+        }   
+    }
+}
---
+++ Pfizer.QueueSystem.Core/Services/QueueSystemManager.cs
@@ -0,0 +1,32 @@
+﻿using Abp.Domain.Repositories;
+using Abp.Domain.Services;
+using Pfizer.QueueSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pfizer.QueueSystem.Services
+{
+    public class QueueSystemManager : DomainService, IQueueSystemManager
+    {
+        private readonly IRepository<QueueHistory> _queueHistoryRepository;
+
+        public QueueSystemManager(IRepository<QueueHistory> queueHistoryRepository)
+        {
+            _queueHistoryRepository = queueHistoryRepository;
+        }
+
+        public async Task<int> GetOnlineCustomersCount(DateTime lastActivityFromUtc)
+        {
+            //TODO
+            //var query = _customerRepository.Table;
+            //query = query.Where(c => lastActivityFromUtc <= c.LastActivityDateUtc);
+
+            var list = await _queueHistoryRepository.GetAllListAsync();
+            var count = list.Count();
+            return count;
+        }
+    }
+}
