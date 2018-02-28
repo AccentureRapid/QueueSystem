c257f79ea02da670e8a7204af66d636a03a7b395
David <david0718@qq.com>
2018-02-28 11:23:23 +0800

    add CanAccessSystem sample for client app

--- WebApiClientSample/Controllers/HomeController.cs
+++ WebApiClientSample/Controllers/HomeController.cs
@@ -13,28 +13,46 @@ namespace WebApiClientSample.Controllers
     {
         public ActionResult Index()
         {
+            #region GetOnlineCustomersCount example
+            //var client = new RestClient("http://localhost:8085");
+            //var request = new RestRequest("api/services/app/queueSystemService/GetOnlineCustomersCount", Method.POST);
+
+
+            //// execute the request
+            //IRestResponse response = client.Execute(request);
+            //var content = response.Content; // raw content as string
+
+            //var jsonObject = JObject.Parse(content);
+            //var success = Convert.ToBoolean(jsonObject["success"].ToString());
+            //var onlineCustomersCount = Convert.ToInt32(jsonObject["result"].ToString());
+
+            //var allowedMaxOnlineCustomerCount = Convert.ToInt32(ConfigurationManager.AppSettings["AllowedMaxOnlineCustomerCount"]);
+            //if (success)
+            //{
+            //    if (onlineCustomersCount > allowedMaxOnlineCustomerCount)
+            //    {
+            //        Response.Redirect(string.Format(@"http://localhost:8085/queue?eid={0}","admin"));
+            //    }
+            //} 
+            #endregion
+
+            #region CanAccessSystem example
             var client = new RestClient("http://localhost:8085");
-            var request = new RestRequest("api/services/app/queueSystemService/GetOnlineCustomersCount", Method.POST);
-           
+            var request = new RestRequest("api/services/app/queueSystemService/CanAccessSystem", Method.POST);
+
 
             // execute the request
             IRestResponse response = client.Execute(request);
             var content = response.Content; // raw content as string
 
             var jsonObject = JObject.Parse(content);
-            var success = Convert.ToBoolean(jsonObject["success"].ToString());
-            var onlineCustomersCount = Convert.ToInt32(jsonObject["result"].ToString());
+            var canAccessSystem = Convert.ToBoolean(jsonObject["result"].ToString());
 
-            var allowedMaxOnlineCustomerCount = Convert.ToInt32(ConfigurationManager.AppSettings["AllowedMaxOnlineCustomerCount"]);
-            if (success)
+            if (!canAccessSystem)
             {
-                if (onlineCustomersCount > allowedMaxOnlineCustomerCount)
-                {
-                    Response.Redirect(string.Format(@"http://localhost:8085/queue?eid={0}","admin"));
-                }
-            }
-
-            
+                Response.Redirect(string.Format(@"http://localhost:8085/queue?eid={0}", "admin"));
+            } 
+            #endregion
 
             return View();
         }
