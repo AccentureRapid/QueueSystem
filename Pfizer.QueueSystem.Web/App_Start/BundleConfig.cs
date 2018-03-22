f5b51fa40488963b1ed8e1df8050e5e875c53cea
David Yu <david0718@qq.com>
2018-03-22 14:29:02 +0800

    update scripts reference

--- Pfizer.QueueSystem.Web/App_Start/BundleConfig.cs
+++ Pfizer.QueueSystem.Web/App_Start/BundleConfig.cs
@@ -48,7 +48,48 @@ namespace Pfizer.QueueSystem.Web
                         "~/js/main.js"
                     )
             );
+            //queue page 
 
+            bundles.Add(
+                new StyleBundle("~/Bundles/queue/css")
+                    .Include("~/fonts/roboto/roboto.css", new CssRewriteUrlTransform())
+                    .Include("~/fonts/material-icons/materialicons.css", new CssRewriteUrlTransform())
+                    .Include("~/lib/bootstrap/dist/css/bootstrap.css", new CssRewriteUrlTransform())
+                    .Include("~/lib/toastr/toastr.css", new CssRewriteUrlTransform())
+                    .Include("~/lib/sweetalert/dist/sweetalert.css", new CssRewriteUrlTransform())
+                    .Include("~/lib/famfamfam-flags/dist/sprite/famfamfam-flags.css", new CssRewriteUrlTransform())
+                    .Include("~/lib/font-awesome/css/font-awesome.css", new CssRewriteUrlTransform())
+                    .Include("~/lib/Waves/dist/waves.css", new CssRewriteUrlTransform())
+                    .Include("~/lib/animate.css/animate.css", new CssRewriteUrlTransform())
+                    .Include("~/css/materialize.css", new CssRewriteUrlTransform())
+                    .Include("~/css/style.css", new CssRewriteUrlTransform())
+                    .Include("~/Views/QueueSystem/_Layout.css", new CssRewriteUrlTransform())
+            );
+
+            bundles.Add(
+                new ScriptBundle("~/Bundles/queue/js/bottom")
+                    .Include(
+                        "~/lib/json2/json2.js",
+                        "~/lib/jquery/dist/jquery.js",
+                        "~/lib/bootstrap/dist/js/bootstrap.js",
+                        "~/lib/moment/min/moment-with-locales.js",
+                        "~/lib/jquery-validation/dist/jquery.validate.js",
+                        "~/lib/blockUI/jquery.blockUI.js",
+                        "~/lib/toastr/toastr.js",
+                        "~/lib/sweetalert/dist/sweetalert-dev.js",
+                        "~/lib/spin.js/spin.js",
+                        "~/lib/spin.js/jquery.spin.js",
+                        "~/lib/Waves/dist/waves.js",
+                        "~/lib/abp-web-resources/Abp/Framework/scripts/abp.js",
+                        "~/lib/abp-web-resources/Abp/Framework/scripts/libs/abp.jquery.js",
+                        "~/lib/abp-web-resources/Abp/Framework/scripts/libs/abp.toastr.js",
+                        "~/lib/abp-web-resources/Abp/Framework/scripts/libs/abp.blockUI.js",
+                        "~/lib/abp-web-resources/Abp/Framework/scripts/libs/abp.spin.js",
+                        "~/lib/abp-web-resources/Abp/Framework/scripts/libs/abp.sweet-alert.js",
+                        "~/js/admin.js",
+                        "~/js/main.js"
+                    )
+            );
             //VENDOR RESOURCES
 
             //~/Bundles/vendor/css
