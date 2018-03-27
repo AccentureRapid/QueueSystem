0da8c08078cee4581aa875f1224a080193db2101
David Yu <david0718@qq.com>
2018-03-27 14:58:05 +0800

    update

--- Pfizer.QueueSystem.Web/Views/QueueSystem/Queue.js
+++ Pfizer.QueueSystem.Web/Views/QueueSystem/Queue.js
@@ -22,8 +22,44 @@
             error: function (data) {
                 console.log('data: ' + data);
             }
-        })
+        });
+
+        $('#btnTake').click(function () {
+            
+            var dto = {};
+            dto.id = $("#timespan-container option:selected" ).val();
+            dto.ntId = getParameterByName('ntid');
+
+            $.ajax({
+            url: "../api/services/app/queueSystemService/TakeFastToken",
+            // 数据发送方式
+            type: "POST",
+            // 接受数据格式
+            dataType: "json",
+            // 要传递的数据
+            data: dto,
+            // 回调函数，接受服务器端返回给客户端的值，即result值
+            success: function (data) {
+               console.log('data received: ' + data);
+            },
+
+            error: function (data) {
+                console.log('data: ' + data);
+            }
+        });
+        function getParameterByName(name, url) {
+                if (!url) url = window.location.href;
+                name = name.replace(/[\[\]]/g, "\\$&");
+                var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                results = regex.exec(url);
+                if (!results) return null;
+                if (!results[2]) return '';
+                return decodeURIComponent(results[2].replace(/\+/g, " "));
+        }
+
+        });
 
     });
+
 
 })();
